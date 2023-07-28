2ba2735fd174618b5bde426d462e417306067cff
usuario <jqncaraballo@gmail.com>
2023-07-28 15:02:02 -0300

    Se agregaron botones en el detail para realizar la compra, suma o resta de los productos

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import ItemCount from "../ItemCount/ItemCount"
 
 
 
 const ItemDetail = ({id, nombre, img, descripcion, category, precio, stock}) => {
+    const [ cantidad, setCantidad ] = useState(1)
+    
+
+    const handleAgregar = () => {
+        const item = {
+            id,
+            nombre, 
+            descripcion, 
+            precio, 
+            img, 
+            stock, 
+            category, 
+            cantidad
+        }
+        console.log(item)
+    }
+
 
     return(
         <div className="container my-5">
@@ -13,6 +32,13 @@ const ItemDetail = ({id, nombre, img, descripcion, category, precio, stock}) => 
             <h4>Precio: ${precio}</h4>
             <br />
             <small>categoría: {category}</small>
+
+            <ItemCount 
+                max={stock}
+                cantidad={cantidad}
+                setCantidad={setCantidad}
+                handleAgregar={handleAgregar}
+            />
         </div>
     )
 }
--- src/componentes/Nosotros/Nosotros.jsx
+++ src/componentes/Nosotros/Nosotros.jsx
@@ -1,20 +1,34 @@
-import { useProductos } from "../../hooks/useProductos"
+import { useEffect } from "react"
+import useMobile from "../../hooks/useMobile"
 
 
 
 const Nosotros = () => {
-    const { productos} = useProductos()
+    const isMobile = useMobile()
 
-    console.log(productos)
+    const clickear = (e) => {
+        console.log(e)
+    }
+
+
+    useEffect(() => {
+        window.addEventListener(`click`, clickear) 
+
+        return () => {
+            window.removeEventListener(`click`, clickear )
+    }
+ 
+}, [])
 
     return(
         <div className="container my-5">
-            <h2>Nosotros</h2>
+            <h2 onClick={clickear}>Nosotros</h2>
             <hr />
 
             {
-                JSON.stringify(productos)
+                isMobile ? <h3>Estamos en mobile</h3> : <p>estamos en desk</p>
             }
+
         </div>
     )
 }
